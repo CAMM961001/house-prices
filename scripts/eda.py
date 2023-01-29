e23bfaf7b7e151d5fe543ea523f2c28552e0def7
CAMM961001 <miguel.castama@gmail.com>
2023-01-29 15:24:59 -0600

    Second EDA figure added to script

--- scripts/eda.py
+++ scripts/eda.py
@@ -13,15 +13,35 @@ with open(os.path.join(ROOT, 'config.yaml'), 'r') as f:
     config = yaml.safe_load(f)
 f.close()
 
-# ---- Data load ----
+# Load data to __file__
 train_data = pd.read_csv(os.path.join(ROOT, config['eda']['train_set']))
 test_data = pd.read_csv(os.path.join(ROOT, config['eda']['test_set']))
 
-#Not a number values
+
+# ---- NAN visualization ----
 fig, ax = plt.subplots(figsize=(25,10))
+
+# Plot specs
 sns.heatmap(data=train_data.isnull(), yticklabels=False, ax=ax)
+
 fig.savefig(os.path.join(ROOT, 'images/nan.jpg'))
 
 
+# ---- Sale price vs house style ----
+fig, ax = plt.subplots(figsize=(25,10))
+
+# Plot specs
+sns.countplot(x=train_data['SaleCondition'])
+sns.histplot(x=train_data['SaleType'], kde=True, ax=ax)
+sns.violinplot(x=train_data['HouseStyle'], y=train_data['SalePrice'], ax=ax)
+sns.scatterplot(x=train_data["Foundation"], y=train_data["SalePrice"], ax=ax)
+
+# Annotations and styling
+ax.set_title('House style effect on sale price')
+plt.grid()
+
+fig.savefig(os.path.join(ROOT, 'images/price_house_style.jpg'))
+
+
 if __name__ == '__main__':
-    print("Hola mundo")
+    print("Job done")
