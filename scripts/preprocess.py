3579a5482d099478c46163a383a4ef40522d2517
CAMM961001 <miguel.castama@gmail.com>
2023-01-31 13:45:40 -0600

    Add feature of interest to __file__

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,9 +1,28 @@
+import os
+
 import numpy as np
 import pandas as pd
 
 from settings import Settings
 
+
+# Initialize project settings
 settings = Settings()
 
+
+
+# Load feature selector
+feature_selector = os.path.join(
+    settings.ROOT_PATH,
+    settings.CONFIG['assets']['feature_selector'])
+
+feature_selector = pd.read_csv(feature_selector)
+
+# Load train data to __file__
+features = feature_selector.loc[feature_selector['consider'] == 1, 'feature']
+train_data = settings.train_data[features.to_list()]
+
+
+
 if __name__ == '__main__':
-    print(settings.ROOT)
+    print(train_data.info())
