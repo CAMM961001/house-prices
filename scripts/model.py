c2450ef76977ce65ede2d770f28bb3b27c2dc9af
CAMM961001 <miguel.castama@gmail.com>
2023-01-31 23:45:52 -0600

    Train time added to log prompt

--- scripts/model.py
+++ scripts/model.py
@@ -1,11 +1,10 @@
-import os
-
 import pandas as pd
 import datetime as dt
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 
+from time import process_time
 from settings import Settings
 
 
@@ -20,16 +19,22 @@ train = settings.processed_train
 
 # Split in features and target
 X = train.drop(columns=target)
-y = train[target]
+y = train[target].values.ravel()
 
 # Hyper-parameters
 max_leaf_nodes = settings.CONFIG['model']['max_leafs']
 n_folds = settings.CONFIG['model']['n_folds']
 
 # Train model
+start = process_time()
+
 model = RandomForestRegressor(max_leaf_nodes=max_leaf_nodes)
 model.fit(X=X, y=y)
-score = cross_val_score(model, X, y, cv=n_folds)
+scores = cross_val_score(model, X, y, cv=n_folds)
+
+stop = process_time()
+
+# Predict
 
 
 if __name__ == '__main__':
@@ -37,14 +42,15 @@ if __name__ == '__main__':
     # Register task in log file
     # log = os.path.join(settings.ROOT_PATH, settings.CONFIG['assets']['pipe_log'])
     
-    # prompt = f"Job: {__file__}\n"
-    # prompt += "Status: DONE\n"
-    # prompt += f"TimeStamp: {dt.datetime.now()}\n\n"
+    prompt = f"Job: {__file__}\n"
+    prompt += "Status: DONE\n"
+    prompt += f"TimeStamp: {dt.datetime.now()}\n"
+    prompt += f"Scores: {scores}\n"
+    prompt += f"MeanScore: {scores.mean()}\n"
+    prompt += f"ProcessTime: {stop - start} [seconds]"
 
     # with open(file=log, mode='a') as f:
     #     f.write(prompt)
     # f.close()
 
-    print(X.shape)
-    print(y.shape)
-    print(score)
+    print(prompt)
