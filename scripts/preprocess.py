5f8c4aa4a71df21dcda5b6048da4e92e5017b935
CAMM961001 <miguel.castama@gmail.com>
2023-01-31 22:40:58 -0600

    Test data added to pipe

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -8,6 +8,39 @@ from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 from settings import Settings
 
 
+def encode_data(df, ord, cat):
+    """
+    
+    """
+    # Ordinal encoding
+    for feat in ord:
+        feat_order = feat_select.loc[feat_select['feature'] == feat, 'order']
+        feat_order = feat_order.values[0].split(";")
+
+        ord_enc = OrdinalEncoder(
+            categories=[feat_order],
+            handle_unknown='use_encoded_value',
+            unknown_value=-1)
+        ord_enc = ord_enc.fit_transform(df[[feat]])
+        df.loc[df.index, feat] = ord_enc
+
+
+    # One hot encoding
+    for feat in cat:
+        ohe = OneHotEncoder(
+            sparse_output=False,
+            handle_unknown='infrequent_if_exist')
+        
+        ohe = pd.DataFrame(
+            ohe.fit_transform(df[[feat]]),
+            columns=ohe.get_feature_names_out())
+
+        df = df.join(ohe)
+        df.drop(columns=feat, inplace=True)
+
+    return df
+
+
 # Initialize project settings
 settings = Settings()
 
@@ -17,7 +50,7 @@ feat_select = os.path.join(
     settings.CONFIG['assets']['feature_selector'])
 
 feat_select = pd.read_csv(feat_select)
-feat_select = feat_select.loc[feat_select['consider'] == 1]
+feat_select = feat_select.loc[feat_select['consider'] != 0]
 
 # List of numerical, categorical and ordinal features
 num = feat_select.loc[feat_select['type_class'] == 'numerical']
@@ -31,35 +64,12 @@ ord = ord['feature'].to_list()
 
 # Load desired features from train data to __file__
 train_data = settings.train_data[feat_select['feature'].to_list()]
+train_data = encode_data(df=train_data, ord=ord, cat=cat)
 
-
-# Ordinal encoding
-for feat in ord:
-    feat_order = feat_select.loc[feat_select['feature'] == feat, 'order']
-    feat_order = feat_order.values[0].split(";")
-
-    ord_enc = OrdinalEncoder(
-        categories=[feat_order],
-        handle_unknown='use_encoded_value',
-        unknown_value=-1)
-    ord_enc = ord_enc.fit_transform(train_data[[feat]])
-    train_data.loc[train_data.index, feat] = ord_enc
-
-
-# One hot encoding
-for feat in cat:
-    ohe = OneHotEncoder(
-        sparse_output=False,
-        handle_unknown='infrequent_if_exist')
-    
-    ohe = pd.DataFrame(
-        ohe.fit_transform(train_data[[feat]]),
-        columns=ohe.get_feature_names_out())
-
-    train_data = train_data.join(ohe)
-    train_data.drop(columns=feat, inplace=True)
-    
-
+# Load desired features from test data to __file__
+test_data = feat_select.loc[feat_select['consider'] == 1]['feature'].to_list()
+test_data = settings.test_data[test_data]
+test_data = encode_data(df=test_data, ord=ord, cat=cat)
 
 if __name__ == '__main__':
 
@@ -68,6 +78,11 @@ if __name__ == '__main__':
         settings.ROOT_PATH,
         settings.CONFIG['assets']['processed_train'])
     train_data.to_csv(processed_train, index=False)
+
+    processed_test = os.path.join(
+        settings.ROOT_PATH,
+        settings.CONFIG['assets']['processed_test'])
+    test_data.to_csv(processed_test, index=False)
 
     # Register task in log file
     log = os.path.join(settings.ROOT_PATH, settings.CONFIG['assets']['pipe_log'])
