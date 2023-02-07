9a7930c3dbd8daec8ec9a310124fbff776968869
CAMM961001 <miguel.castama@gmail.com>
2023-02-07 11:50:18 -0600

    Feature selector added to settings script

--- scripts/settings.py
+++ scripts/settings.py
@@ -8,20 +8,27 @@ class Settings:
     def __init__(self):
         self.SCRIPTS_PATH = os.path.dirname(os.path.abspath(__file__))
         self.ROOT_PATH = os.path.dirname(self.SCRIPTS_PATH)
-
+        
         with open(os.path.join(self.ROOT_PATH, "config.yaml"), 'r') as f:
             self.CONFIG = yaml.safe_load(f)
         f.close()
 
-        self.target = os.path.join(
+        self.FEAT_PATH = os.path.join(
             self.ROOT_PATH,
             self.CONFIG['assets']['feature_selector'])
-        self.target = read_csv(self.target)
-        self.target = self.target.loc[self.target['consider'] == 2, 'feature'].to_list()
+
+        # Feature selector
+        self.feature_selector = read_csv(self.FEAT_PATH)
+        self.feature_selector = self.feature_selector.loc[self.feature_selector['consider'] != 0]
+
+        # Target feature
+        self.target = self.feature_selector.loc[
+            (self.feature_selector['consider'] == 2, 'feature')].to_list()
 
     def __str__(self) -> str:
         prompt = f"SCRIPTS_PATH:\t{self.SCRIPTS_PATH}\n"
         prompt += f"ROOT_PATH:\t{self.ROOT_PATH}"
+        prompt += f"FEAT_PATH:\t{self.FEAT_PATH}"
         return prompt
 
     def invoque_data(self, env_var):
