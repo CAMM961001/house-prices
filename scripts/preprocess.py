1198bfa97476122d65d9ca3139d8e8ba7dc2e525
CAMM961001 <miguel.castama@gmail.com>
2023-02-07 12:17:23 -0600

    Save data with setting function

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -7,12 +7,20 @@ from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
 from settings import Settings
 
 
-def encode_data(df, ord, cat):
+def encode_data(df, feat_select):
     """
     
     """
+    # Dictionary of numerical, categorical and ordinal features
+    feats = {"numerical":None, "categorical":None, "ordinal":None}
+
+    for key in feats.keys():
+        ftype = feat_select.loc[feat_select['type_class'] == key]
+        feats[key] = ftype['feature'].to_list()
+    
+    
     # Ordinal encoding
-    for feat in ord:
+    for feat in feats['ordinal']:
         feat_order = feat_select.loc[feat_select['feature'] == feat, 'order']
         feat_order = feat_order.values[0].split(";")
 
@@ -25,7 +33,7 @@ def encode_data(df, ord, cat):
 
 
     # One hot encoding
-    for feat in cat:
+    for feat in feats['categorical']:
         cats = feat_select.loc[feat_select['feature'] == feat, 'order']
         cats = cats.values[0].split(";")
 
@@ -49,44 +57,32 @@ def encode_data(df, ord, cat):
 settings = Settings()
 
 # Load feature selector data
-feat_select = settings.feature_selector
-
-# Dictionary of numerical, categorical and ordinal features
-feats = {"numerical":None, "categorical":None, "ordinal":None}
-for key in feats.keys():
-    ftype = feat_select.loc[feat_select['type_class'] == key]
-    feats[key] = ftype['feature'].to_list()
+feature_selector = settings.feature_selector
 
 
 # Load desired features from TRAIN data to __file__
 train_data = settings.invoque_data(env_var='train_set')
-train_data = train_data[feat_select['feature'].to_list()]
+train_data = train_data[feature_selector['feature'].to_list()]
 
 # Train set encoding
-train_data = encode_data(df=train_data, ord=feats['ordinal'], cat=feats['categorical'])
+train_data = encode_data(df=train_data, feat_select=feature_selector)
 train_data.dropna(axis=0, inplace=True)
 
 # Save processed data in assets directory
-processed_train = os.path.join(
-    settings.ROOT_PATH,
-    settings.CONFIG['assets']['processed_train'])
-train_data.to_csv(processed_train, index=False)
+settings.save_df(df=train_data, env_var='processed_train')
 
 
 # Load desired features from TEST data to __file__
-test_feats = feat_select.loc[feat_select['consider'] == 1]['feature'].to_list()
+test_feats = feature_selector.loc[feature_selector['consider'] == 1]['feature'].to_list()
 test_data = settings.invoque_data(env_var='test_set')
 test_data = test_data[test_feats]
 
 # Test set encoding
-test_data = encode_data(df=test_data, ord=feats['ordinal'], cat=feats['categorical'])
+test_data = encode_data(df=test_data, feat_select=feature_selector)
 test_data.dropna(axis=0, inplace=True)
 
 # Save processed data in assets directory
-processed_test = os.path.join(
-    settings.ROOT_PATH,
-    settings.CONFIG['assets']['processed_test'])
-test_data.to_csv(processed_test, index=False)
+settings.save_df(df=train_data, env_var='processed_test')
 
 
 if __name__ == '__main__':
